f101891945de537461f8b0ce1a684c93395456f3
Alexander Lätsch <info@laetsch-it.de>
2019-07-05 20:34:58 +0200

    disable via property

--- src/main/java/de/lit/jobscheduler/JobSchedulerAutoConfiguration.java
+++ src/main/java/de/lit/jobscheduler/JobSchedulerAutoConfiguration.java
@@ -4,6 +4,7 @@ import de.lit.jobscheduler.dao.JobDefinitionDao;
 import de.lit.jobscheduler.dao.JobExecutionDao;
 import de.lit.jobscheduler.entity.JobDefinition;
 import de.lit.jobscheduler.entity.JobExecution;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +13,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @ComponentScan
+@ConditionalOnProperty(name = "application.jobscheduler.enable", matchIfMissing = true)
 @EnableJpaRepositories(basePackageClasses = {JobDefinitionDao.class, JobExecutionDao.class})
 @EntityScan(basePackageClasses = {JobDefinition.class, JobExecution.class})
 @EnableScheduling
